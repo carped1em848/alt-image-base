27e317c8f487e0e672d58c82782c3774b4fd5dde
dm <dmitry@udalov.online>
2025-02-11 05:22:03 +0600

    fix: fix comments

--- src/main.sh
+++ src/main.sh
@@ -4,14 +4,12 @@ set -e
 
 echo "Running scripts"
 
-# Список директорий, в которых лежат скрипты (в нужном порядке)
 directories=(
   "/src/packages"
   "/src/configuration"
   "/src/make"
 )
 
-# Функция для запуска скриптов внутри заданной папки
 run_scripts_in_dir() {
   local dir="$1"
   echo "=== Running scripts in $dir ==="
@@ -27,7 +25,6 @@ run_scripts_in_dir() {
   fi
 }
 
-# Запускаем все скрипты в перечисленных директориях
 for d in "${directories[@]}"; do
   run_scripts_in_dir "$d"
 done
