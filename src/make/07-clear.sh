068205daaa4cfa017d772f7a2a533ae21495ae31
dm <dmitry@udalov.online>
2025-02-11 03:40:00 +0600

    test: clear boot

--- src/make/07-clear.sh
+++ src/make/07-clear.sh
@@ -5,6 +5,7 @@ echo "::group:: ===$(basename "$0")==="
 rm -rf /var/root/.cargo
 rm -rf /var/root/.rustup
 rm -rf /var/root/.cache
+rm -rf /var/root/.profile
 rm -rf /var/root/go
 rm -rf /boot/*
 truncate -s 0 /var/log/lastlog
