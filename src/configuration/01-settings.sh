bb3d9ee1df85928498826273793267aa97513540
dm <dmitry@udalov.online>
2025-03-22 18:25:32 +0600

    feature: add tmp.mount

--- src/configuration/01-settings.sh
+++ src/configuration/01-settings.sh
@@ -59,6 +59,7 @@ systemctl enable brew-setup.service
 systemctl enable brew-upgrade.timer
 systemctl enable brew-update.timer
 systemctl enable update-image-task.timer
+systemctl enable tmp.mount
 
 # Расширение лимитов на число открытых файлов для всех юзеров. (при обновлении системы открывается большое число файлов/слоев)
 grep -qE "^\* hard nofile 978160$" /etc/security/limits.conf || echo "* hard nofile 978160" >> /etc/security/limits.conf
