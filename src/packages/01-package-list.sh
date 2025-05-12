e5e1e20a5a5cb7e06d67b8929a6a2bfa66a9a488
Vladimir Vaskov <rirusha@altlinux.org>
2025-05-12 23:25:40 +0300

    Remove bootupd from make

--- src/packages/01-package-list.sh
+++ src/packages/01-package-list.sh
@@ -4,115 +4,116 @@ echo "::group:: ===$(basename "$0")==="
 
 # --- Базовые утилиты и консольные инструменты ---
 BASE_UTILS=(
-    "tzdata"
-    "eza"
-    "man"
-    "mc"
-    "nano"
-    "passwd"
-    "bubblewrap"
-    "which"
-    "su"
-    "sudo"
-    "curl"
-    "wget"
-    "unzip"
-    "util-linux"
-    "coreutils"
-    "iputils"
-    "rsync"
+    tzdata
+    eza
+    man
+    mc
+    nano
+    passwd
+    bubblewrap
+    which
+    su
+    sudo
+    curl
+    wget
+    unzip
+    util-linux
+    coreutils
+    iputils
+    rsync
 )
 
 # --- Пакеты для контейнеров (Docker / Podman / Flatpak и т.д.) ---
 CONTAINER_PACKAGES=(
-    "bootc"
-    "distrobox"
-    "flatpak"
-    "docker-engine"
-    "podman"
-    "containers-common"
-    "fuse-overlayfs"
-    "composefs"
-    "skopeo"
+    bootc
+    bootupd
+    distrobox
+    flatpak
+    docker-engine
+    podman
+    containers-common
+    fuse-overlayfs
+    composefs
+    skopeo
 )
 
 # --- Пакеты для разработки: Go, Rust, C/C++, системные dev-библиотеки и т.п. ---
 DEV_PACKAGES=(
-    "rust-cargo"
-    "golang"
-    "rust"
-    "build-essential"
-    "pkg-config"
-    "openssl"
-    "openssl-devel"
-    "glib2"
-    "glib2-devel"
-    "glibc-utils"
-    "systemd-devel"
-    "libgio"
-    "libgio-devel"
-    "git"
-    "libostree-devel"
+    rust-cargo
+    golang
+    rust
+    build-essential
+    pkg-config
+    openssl
+    openssl-devel
+    glib2
+    glib2-devel
+    glibc-utils
+    systemd-devel
+    libgio
+    libgio-devel
+    git
+    libostree-devel
 )
 
 # --- Утилиты для загрузки / EFI / Boot ---
 BOOT_PACKAGES=(
-    "efivar"
-    "shim-unsigned"
-    "shim-signed"
-    "efitools"
-    "efibootmgr"
-    "grub"
-    "grub-efi"
-    "grub-btrfs"
-    "dracut"
+    efivar
+    shim-unsigned
+    shim-signed
+    efitools
+    efibootmgr
+    grub
+    grub-efi
+    grub-btrfs
+    dracut
 )
 
 # --- Ядро и связанные модули ---
 KERNEL_PACKAGES=(
-    "kernel-image-6.12"
-    "kernel-modules-drm-6.12"
+    kernel-image-6.12
+    kernel-modules-drm-6.12
 )
 
 # --- Виртуализация и гостевые агенты (QEMU, Spice, LXD/Libvirt и т.д.) ---
 VIRT_PACKAGES=(
-    "open-vm-tools"
-    "qemu-guest-agent"
-    "spice-vdagent"
-    "virtiofsd"
-    "libvirt"
-    "lxd"
+    open-vm-tools
+    qemu-guest-agent
+    spice-vdagent
+    virtiofsd
+    libvirt
+    lxd
 )
 
 # --- Системные библиотеки, инструменты и утилиты ---
 SYSTEM_TOOLS=(
-    "firmware-linux"
-    "fprintd"
-    "jq"
-    "yq"
-    "mount"
-    "policycoreutils"
-    "libselinux"
-    "losetup"
-    "dosfstools"
-    "e2fsprogs"
-    "NetworkManager"
-    "sfdisk"
-    "bluez"
-    "btrfs-progs"
-    "kbd"
-    "kbd-data"
-    "ostree"
-    "systemd"
-    "chrony"
-    "plymouth"
-    "attr"
+    firmware-linux
+    fprintd
+    jq
+    yq
+    mount
+    policycoreutils
+    libselinux
+    losetup
+    dosfstools
+    e2fsprogs
+    NetworkManager
+    sfdisk
+    bluez
+    btrfs-progs
+    kbd
+    kbd-data
+    ostree
+    systemd
+    chrony
+    plymouth
+    attr
 )
 
 # --- Графические пакеты и драйверы ---
 GRAPHICS_PACKAGES=(
-    "mesa-dri-drivers"
-    "glxinfo"
+    mesa-dri-drivers
+    glxinfo
 )
 
 # Теперь объединим всё в один список:
